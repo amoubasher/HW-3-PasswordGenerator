1d24abaa75d1784f8ac98befbcec32a7ab92a90d
Amr Moubasher <amoubasher@gmail.com>
2022-10-13 16:22:08 -0700

    Checked for password length, not done yet

--- Develop/script.js
+++ Develop/script.js
@@ -31,15 +31,21 @@ function getRandomInt(max) {
 // Step 1 Ask how many characters the user wants the password to be (8-12 characters)
 // Step 2 Ask user if the user wants to use uppercase, lower case, numbers, & special characters
 function generatePassword () {
-  var passwordLength = prompt("How many characters do you want your password to be? (8-12)", "12");
+  var passwordLength = prompt("How many characters do you want your password to be? (8-128)", "12");
   var useUpperCase = confirm("Would you like to include uppercase letters in your password?");
   var useLowerCase = confirm("Would you like to include lowercase letters in your password?");
   var useNumbers = confirm("Would you like to include numers in your password?");
   var useSpecialChars = confirm("Would you like to include special characters in your password?");
 
   // Put options into a datastructure so we can use user input to select a random character for the length they choose. 
-  // Make a structure for potential characters from arrys of characters
+  // Make a structure for potential characters from arrays of characters
   var potentialChars = [];
+
+  if (passwordLength > 128 || passwordLength < 8) {
+    prompt("Invalid input: Please enter a value between 8 & 128");
+    return;
+  }
+
   if (useUpperCase) {
     potentialChars = potentialChars.concat(uppercase);
   }
@@ -57,11 +63,12 @@ function generatePassword () {
   }
 
   // Create the password to be the length of passwordLength
+
   var password = "";
   for (var i=0; i < passwordLength; i++) {
     password = password + potentialChars[getRandomInt(potentialChars.length)]
   }
-  return password
+  return password;
 
-
+  // Make the user have to input a number between 8 & 128 characters, if  < 8 or >128; tell them its wrong
 }
