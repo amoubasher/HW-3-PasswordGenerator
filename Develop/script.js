d90e4e4f902ff8d4d3c2f70d208abe8e2fb69044
Amr Moubasher <amoubasher@gmail.com>
2022-10-13 17:01:39 -0700

    added criteria for length, comments for what needs to be done still

--- Develop/script.js
+++ Develop/script.js
@@ -32,19 +32,29 @@ function getRandomInt(max) {
 // Step 2 Ask user if the user wants to use uppercase, lower case, numbers, & special characters
 function generatePassword () {
   var passwordLength = prompt("How many characters do you want your password to be? (8-128)", "12");
+
+  // If the user chooses > 128 or < 8 characters, dont execute the rest of the code and let them know their input was invalid
+
+  if (passwordLength > 128 || passwordLength < 8) {
+    prompt("Invalid input: Please enter a value between 8 & 128");
+    return generatePassword();
+  }
+
   var useUpperCase = confirm("Would you like to include uppercase letters in your password?");
   var useLowerCase = confirm("Would you like to include lowercase letters in your password?");
   var useNumbers = confirm("Would you like to include numers in your password?");
   var useSpecialChars = confirm("Would you like to include special characters in your password?");
 
+  // Add code here to make sure that the user chose at least one of the criteria, if they choose no criteria tell them it is invalid and show in the box
+
+  if (useUpperCase === false && useLowerCase === false && useNumbers === false && useSpecialChars === false) {
+    prompt("You have not chosen any criteria for your password. Please select at least one criteria");
+    return generatePassword();
+  }
+
   // Put options into a datastructure so we can use user input to select a random character for the length they choose. 
   // Make a structure for potential characters from arrays of characters
   var potentialChars = [];
-
-  if (passwordLength > 128 || passwordLength < 8) {
-    prompt("Invalid input: Please enter a value between 8 & 128");
-    return;
-  }
 
   if (useUpperCase) {
     potentialChars = potentialChars.concat(uppercase);
@@ -62,7 +72,7 @@ function generatePassword () {
     potentialChars = potentialChars.concat(special);
   }
 
-  // Create the password to be the length of passwordLength
+  // Add code here that will check to make sure all the user's selected criteria will show up in the password
 
   var password = "";
   for (var i=0; i < passwordLength; i++) {
